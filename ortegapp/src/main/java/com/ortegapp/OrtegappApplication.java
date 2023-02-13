ae811fc48b5f21cd243cf5a3c340484d56c54f8f
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2023-02-13 11:39:13 +0100

    commit inicial

---
+++ ortegapp/src/main/java/com/ortegapp/OrtegappApplication.java
@@ -0,0 +1,13 @@
+package com.ortegapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrtegappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrtegappApplication.class, args);
+	}
+
+}
---
+++ ortegapp/src/test/java/com/ortegapp/OrtegappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortegapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrtegappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
