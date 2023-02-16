05702251e229b48b9783780e59cf589f02caf890
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2023-02-16 16:41:22 +0100

    comentario casi terminado

--- ortegapp/src/main/java/com/ortegapp/service/UserService.java
+++ ortegapp/src/main/java/com/ortegapp/service/UserService.java
@@ -94,5 +94,9 @@ public class UserService {
         return passwordEncoder.matches(clearPassword, user.getPassword());
     }
 
+    public boolean userExists(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
 
 }
---
+++ ortegapp/src/main/java/com/ortegapp/validation/validator/UniqueUsernameValidator.java
@@ -0,0 +1,21 @@
+package com.ortegapp.validation.validator;
+
+import com.ortegapp.service.UserService;
+import com.ortegapp.validation.annotation.UniqueUsername;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return StringUtils.hasText(s) && !userService.userExists(s);
+    }
+}
