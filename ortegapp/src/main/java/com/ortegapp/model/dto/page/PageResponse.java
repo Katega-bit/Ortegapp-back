076c56a546e734f63079a05cb2cbcb9b879739cd
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2023-02-21 23:53:47 +0100

    cambios varios

--- ortegapp/src/main/java/com/ortegapp/model/dto/page/PageResponse.java
+++ ortegapp/src/main/java/com/ortegapp/model/dto/page/PageResponse.java
@@ -13,19 +13,14 @@ public class PageResponse<T> {
 
     private List<T> content;
 
-    private boolean first;
-
-    private boolean last;
-
-    private int totalPages;
-
     private long totalElements;
+    private int totalPages;
+    private int page;
 
     public  PageResponse(Page<T> page){
         this.content = page.getContent();
-        this.first = page.isFirst();
-        this.last = page.isLast();
         this.totalPages = page.getTotalPages();
+        this.page = page.getNumber();
         this.totalElements = page.getTotalElements();
     }
 
